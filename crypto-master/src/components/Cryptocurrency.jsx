6fbd29d523051966b1b63fdb3f9100c25c88eee5
srosama <srosamazaid@gmail.com>
2023-08-20 16:31:28 +0300

    update

--- crypto-master/src/components/Cryptocurrency.jsx
+++ crypto-master/src/components/Cryptocurrency.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import millify from 'millify';
+import { Link } from 'react-router-dom';
+import { Card, Row, Col, Input } from 'antd';
+import { useGetCryptosQuery }  from '../services/CoinrankingAPI'
+
+
 
 const Cryptocurrency = () => {
+  const {data:coinList, isFetching} = useGetCryptosQuery();
+  const [coins, setCoins] = useState(coinList?.data?.coins)
+  console.log(coins)
   return (
-    <div>Cryptocurrency</div>
+    <>
+    <h1>crypto-card-container</h1>
+    <Row gutter={[32, 32]} className='crypto-card-container'>
+      {coins.map((currency) => (
+        <Col xs={24} sm={12} lg={6} className='crypto-card' key={currency.id}>
+            <Link to={`cys/${currency.id}`}>
+              <Card title={`${currency.rank}. ${currency.name}`}
+               extra={<img className='crypto-image' src={currency.iconUrl}></img>}
+               hoverable={true}
+               >
+               <p>Price: {millify(currency.price)}</p>
+               <p>Market Cap: {millify(currency.marketCap)}</p>
+               <p>Daily Change: {millify(currency.change)}</p>
+              </Card>
+            </Link>
+        </Col>
+      ))}
+    </Row>
+    </>
   )
 }
 
--- crypto-master/src/components/Homepage.jsx
+++ crypto-master/src/components/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import millify from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 import { useGetCryptosQuery }  from '../services/CoinrankingAPI'
-
+import millify from 'millify';
 const { Title } = Typography;
+import Cryptocurrency from './Cryptocurrency'
+import News from './News'
+
 
 const Homepage = () => {
 
@@ -19,12 +21,23 @@ const Homepage = () => {
     <>
     <Title level={2} className='heading'>International Crypto States</Title>
     <Row>
-      <Col span={12}><Statistic title="Totoal Cryptocurrency" value={5}></Statistic></Col>
-      <Col span={12}><Statistic title="Totoal Exchanges" value={5}></Statistic></Col>
-      <Col span={12}><Statistic title="Totoal Market Cap" value={5}></Statistic></Col>
-      <Col span={12}><Statistic title="Totoal 24h Volume" value={5}></Statistic></Col>
-      <Col span={12}><Statistic title="Totoal Markets" value={5}></Statistic></Col>
+      <Col span={12}><Statistic title="Totoal Cryptocurrency" value={globalStats.total}></Statistic></Col>
+      <Col span={12}><Statistic title="Totoal Exchanges" value={millify(globalStats.totalExchanges)}></Statistic></Col>
+      <Col span={12}><Statistic title="Totoal Market Cap" value={millify(globalStats.totalMarketCap)}></Statistic></Col>
+      <Col span={12}><Statistic title="Totoal 24h Volume" value={millify(globalStats.total24hVolume)}></Statistic></Col>
+      <Col span={12}><Statistic title="Totoal Markets" value={millify(globalStats.totalMarkets)}></Statistic></Col>
     </Row>
+    <div className="home-heading-container">
+        <Title level={2} className="home-title">Top 10 Cryptos In The World</Title>
+        <Title level={3} className="show-more"><Link to="/cys" className="show-more">Show more</Link></Title>
+      </div>
+      <Cryptocurrency simplified />
+      
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">Latest Crypto News</Title>
+        <Title level={3}><Link to="/news">Show more</Link></Title>
+      </div>
+      <News simplified />
     </>
   );
 };

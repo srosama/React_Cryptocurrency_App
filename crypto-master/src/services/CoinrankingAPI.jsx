fd3d71c5fab2729a863315ea5e22c80d64bebe00
srosama <srosamazaid@gmail.com>
2023-08-20 17:30:48 +0300

    update

--- crypto-master/src/services/CoinrankingAPI.jsx
+++ crypto-master/src/services/CoinrankingAPI.jsx
@@ -15,7 +15,7 @@ export const cryptoApi = createApi({
   endpoints: (builder) => ({
     getCryptos: builder.query({
       query: () => createRequest(`/coins`),
-    }),
+        }),
     })
 });
 

b2ccb72bf78431c4cb6729012db447358d103b1e
srosama <srosamazaid@gmail.com>
2023-08-21 06:10:11 +0300

    update

--- crypto-master/src/services/CoinrankingAPI.jsx
+++ crypto-master/src/services/CoinrankingAPI.jsx
@@ -16,6 +16,20 @@ export const cryptoApi = createApi({
     getCryptos: builder.query({
       query: (count) => createRequest(`/coins?limit=${count}`),
         }),
+
+        getCryptoDetails: builder.query({
+          query: (coinId) => createRequest(`/coin/${coinId}`),
+        }),
+    
+        // Note: Change the coin price history endpoint from this - `coin/${coinId}/history/${timeperiod} to this - `coin/${coinId}/history?timeperiod=${timeperiod}`
+        getCryptoHistory: builder.query({
+          query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`),
+        }),
+    
+        // Note: To access this endpoint you need premium plan
+        getExchanges: builder.query({
+          query: () => createRequest('/exchanges'),
+        }),
     })
 });
 
@@ -23,4 +37,7 @@ export const cryptoApi = createApi({
 
 export const {
   useGetCryptosQuery,
+  useGetCryptoDetailsQuery,
+  useGetExchangesQuery,
+  useGetCryptoHistoryQuery,
 } = cryptoApi;
